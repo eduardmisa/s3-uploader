c2fb5e0a3ea0e71749bd412828a59942690ddb3f
eduardmisa <eduardmisa@gmail.com>
2025-08-11 23:20:24 +0800

    seo

--- s3-uploader-ui/config/site.ts
+++ s3-uploader-ui/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Emisa File Manager",
+  description: "Emisa File Manager – A secure, fast, and easy-to-use cloud platform to upload, organize, and share files anytime, anywhere.",
   navItems: [
     {
       label: "Home",
