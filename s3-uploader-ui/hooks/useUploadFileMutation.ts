edac274e31d62710e3c7862f6afbbee0c97ebb03
eduardmisa <eduardmisa@gmail.com>
2025-08-12 09:23:49 +0800

    fix

--- s3-uploader-ui/hooks/useUploadFileMutation.ts
+++ s3-uploader-ui/hooks/useUploadFileMutation.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { uploadFileToS3 } from "@/lib/aws-s3";
 
@@ -8,11 +8,9 @@ interface UploadMutationVariables {
 }
 
 export const useUploadFileMutation = (filePathPrefix?: string) => {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: async ({ file, onProgress }: UploadMutationVariables) => {
       return uploadFileToS3(file, onProgress, filePathPrefix);
-    }
+    },
   });
 };
